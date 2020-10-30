2f4f0dddd8c935d6b682fd0def131ac6c98d266a
Thada Wangthammang <mildronize@gmail.com>
2020-10-31 01:08:33 +0700

    change export default to export

--- src/middlewares/validateType.ts
+++ src/middlewares/validateType.ts
@@ -3,7 +3,7 @@ import { validate, ValidationError } from 'class-validator';
 import { RequestHandler } from 'express';
 import { HttpException } from '..';
 
-function validationMiddleware(type: any, skipMissingProperties = false): RequestHandler {
+export function validationMiddleware(type: any, skipMissingProperties = false): RequestHandler {
   return (req, res, next) => {
     validate(plainToClass(type, req.body), { skipMissingProperties }).then((errors: ValidationError[]) => {
       if (errors.length > 0) {
@@ -15,5 +15,3 @@ function validationMiddleware(type: any, skipMissingProperties = false): Request
     });
   };
 }
-
-export default validationMiddleware;
