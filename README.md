660f135205a2aeafff163256af35a462c1226091
Thada Wangthammang <mildronize@gmail.com>
2020-10-30 23:59:28 +0700

    add http-status-codes

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 [![Build Status](https://travis-ci.org/mildronize/route-controller.svg?branch=main)](https://travis-ci.org/mildronize/route-controller)
 
+## Features
+
+- built-in [http-status-codes](https://github.com/prettymuchbryce/http-status-codes)
+
 ## Installation
 
 1. Install the module
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-// import  './decorator.interface';
 export * from './decorator.interface';
 
 export * from './decorators/controller';
@@ -9,5 +8,9 @@ export * from './decorators/utils';
 
 export * from './add-express-controller';
 
-// import  './decorator.interface';
+// Export all HTTP status codes from `http-status-codes` package.
+// Based on the Java Apache HttpStatus API. (http://hc.apache.org/httpclient-3.x/apidocs/org/apache/commons/httpclient/HttpStatus.html)
+
+export * from 'http-status-codes';
+
 // thanks router decorator from https://nehalist.io/routing-with-typescript-decorators/
