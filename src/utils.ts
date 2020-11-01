51aed9b9f8f0cf17e189e786cacf533ddd04a72e
Thada Wangthammang <mildronize@gmail.com>
2020-11-01 23:39:18 +0700

    add utils: response format

---
+++ src/utils.ts
@@ -0,0 +1,29 @@
+import { Response } from 'express';
+
+interface ObjectKeyString {
+    [key: string]: any
+}
+
+interface Option {
+    code?: number;
+    message?: string;
+    data?: any;
+    status?: string;
+}
+
+export function responseFormat(res: Response, opt: Option) {
+    const statusCode = opt.code || 200;
+    const status = opt.status || "success";
+
+    const responseBody: ObjectKeyString = { status }
+    
+    if(opt.data && opt.data !== undefined){
+        responseBody['data'] = opt.data;
+    }
+
+    if(opt.message && opt.message !== undefined){
+        responseBody['message'] = opt.message;
+    }
+
+    res.status(statusCode).json(responseBody);
+}
