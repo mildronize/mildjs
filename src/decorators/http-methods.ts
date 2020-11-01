d835e9d11259f5f3859d0ea0f260e4a11e2d654b
Thada Wangthammang <mildronize@gmail.com>
2020-11-01 18:57:43 +0700

    refactor code

--- src/decorators/http-methods.ts
+++ src/decorators/http-methods.ts
@@ -1,21 +1,6 @@
 import { httpMethodDecorator } from './utils';
 
-export const Get = (path: string): MethodDecorator => {
-  // `target` equals our class, `propertyKey` equals our decorated method name
-  return httpMethodDecorator('get', { path });
-};
-
-export const Post = (path: string): MethodDecorator => {
-  // `target` equals our class, `propertyKey` equals our decorated method name
-  return httpMethodDecorator('post', { path });
-};
-
-export const Put = (path: string): MethodDecorator => {
-  // `target` equals our class, `propertyKey` equals our decorated method name
-  return httpMethodDecorator('put', { path });
-};
-
-export const Delete = (path: string): MethodDecorator => {
-  // `target` equals our class, `propertyKey` equals our decorated method name
-  return httpMethodDecorator('delete', { path });
-};
+export const Get = (path: string): MethodDecorator => httpMethodDecorator('get', { path });
+export const Post = (path: string): MethodDecorator => httpMethodDecorator('post', { path });
+export const Put = (path: string): MethodDecorator => httpMethodDecorator('put', { path });
+export const Delete = (path: string): MethodDecorator => httpMethodDecorator('delete', { path });
