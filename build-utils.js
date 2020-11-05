a8291b67802bb52abdac7a3876a65a60836a2815
Thada Wangthammang <mildronize@gmail.com>
2020-11-05 16:51:50 +0700

    update script gulp

--- build-utils.js
+++ build-utils.js
@@ -19,23 +19,21 @@ function executeStream(...commands) {
   const command = commands.join(' && ');
 
   return new Promise((resolve, reject) => {
-
     const myShellScript = exec(command);
-  
+    console.debug('Running ... ' + command);
+
     myShellScript.stdout.on('data', (data) => {
       console.log(data);
     });
     myShellScript.stderr.on('data', (data) => {
       console.error(data);
     });
-  
+
     myShellScript.on('exit', function (code) {
       console.log('child process exited with code ' + code.toString());
       resolve();
     });
-
   });
-
 }
 
 // exports.runn = runn;
--- gulpfile.js
+++ gulpfile.js
@@ -9,11 +9,33 @@ const test = async () => await run(scope, 'npm test');
 const testCoverage = async () => await run(scope, 'npm run test:coverage');
 const uploadCoverage = async () => await run(scope, 'bash -c "bash <(curl -s https://codecov.io/bash)"');
 
-// exports.build = build;
-exports.default = series(
-  before,
-  build,
-  test,
-  testCoverage,
-  uploadCoverage
-);
+// function buildCI() {
+//   return series(before, build, test, testCoverage, uploadCoverage);
+// }
+// const buildCI =  () =>  series(before, build, test, testCoverage, uploadCoverage);
+
+const start = async () => await run(scope, 'npm start');
+const install = async () => await run(scope, 'npm install');
+
+const npmRun = async (cmd) => await run(scope, 'npm run ' + cmd);
+const npm = async (cmd) => await run(scope, 'npm ' + cmd);
+
+const main = (exit) => {
+  const args = process.argv;
+  const cmd = args[3];
+  switch(args[2]){
+    case '--run': 
+      npmRun(cmd);
+      exit();
+    case '--npm':
+      npm(cmd);
+      exit();
+  }  
+  exit();
+}
+
+exports.install = install;
+exports.start = start;
+exports.build = build;
+exports.default = series(main);
+exports.buildCI = series(before, build, test, testCoverage, uploadCoverage);
